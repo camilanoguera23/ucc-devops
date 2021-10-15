d5434bb774d1ccee541bd6462bfa0e195fa5c686
Camila Andrea Noguera Poveda <camilanoguera23@gmail.com>
2021-10-14 19:48:21 -0500

    cambios en programa.cpp

--- programa.cpp
+++ programa.cpp
@@ -3,11 +3,10 @@
 int main()
 {
     int opcion;
-<<<<<<< HEAD
-    float area=3, l1=5, radio=8, h=2, base=7;
-=======
+ float area=3, l1=5, radio=8, h=2, base=7;
+
     float area, l1, radio, h, base;
->>>>>>> 6569f35497b262f1a15226d5664a0470a1c31c07
+
     printf(" ----------------------------------------------------------------\n");
     printf("|Universidad cooperativa de Colombia                             |\n");
     printf("|Camila Andrea Noguera Poveda                                    |\n");
@@ -16,7 +15,7 @@ int main()
     printf("        1.  Circulo     \n");
     printf("        2. Triangulo    \n");
     printf("        3. Cuadrado     \n");
-<<<<<<< HEAD
+
     //printf("Por favor introduce una opcion: " );
     //scanf("%d", &opcion);
     //switch(opcion)
@@ -25,7 +24,6 @@ int main()
         //{
             printf("Ingresa el radio: %f " , radio);
             //scanf("%f", &radio);
-=======
     printf("Por favor introduce una opcion: " );
     scanf("%d", &opcion);
     switch(opcion)
@@ -34,12 +32,10 @@ int main()
         {
             printf("Ingresa el radio: ");
             scanf("%f", &radio);
->>>>>>> 6569f35497b262f1a15226d5664a0470a1c31c07
 
             area=(radio*radio)*3.14;
             printf("El area del circulo es de : %f " , area);
 
-<<<<<<< HEAD
           //  break;
         //}
 
@@ -50,7 +46,6 @@ int main()
 
             printf("Ingresa altura : %f ", h );
             //scanf("%f", &h);
-=======
             break;
         }
 
@@ -61,12 +56,12 @@ int main()
 
             printf("Ingresa altura : " );
             scanf("%f", &h);
->>>>>>> 6569f35497b262f1a15226d5664a0470a1c31c07
+
 
             area=(base*h)/2;
             printf("El area del triangulo es de : %f " , area);
 
-<<<<<<< HEAD
+
             //break;
         //}
 
@@ -74,28 +69,25 @@ int main()
         //{
 	 printf("Ingresa lado: %f ", l1);
            // scanf("%f", &l1);
-=======
-            break;
+           break;
         }
 
         case 3:
         {
 	 printf("Ingresa lado: ");
             scanf("%f", &l1);
->>>>>>> 6569f35497b262f1a15226d5664a0470a1c31c07
 
             area=l1*l1;
             printf("El area del cuadrado es de : %f ", area);
 
-<<<<<<< HEAD
-          //  break;
+         //  break;
 
         //}
 
        // default:
           //  printf("La opcion no es correcta");
     //}
-=======
+
             break;
 
         }
@@ -103,6 +95,5 @@ int main()
         default:
             printf("La opcion no es correcta");
     }
->>>>>>> 6569f35497b262f1a15226d5664a0470a1c31c07
 	
 }
