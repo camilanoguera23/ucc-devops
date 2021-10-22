b80e61a5b5c11fcc7de246e44da2f6908bd111ae
Camila Andrea Noguera Poveda <camilanoguera23@gmail.com>
2021-10-21 20:32:55 -0500

    particionamiento de programa.cpp en programa_parte1.cpp y programa_parte2.cpp

---
+++ programa_parte2.cpp
@@ -0,0 +1,6 @@
+	//Archivo funcion
+	//vf_area calcular raiz cuadrada en una variable e imprimir el resultado
+	raiz=sqrt(vf_area);
+	cout <<"La raiz cuadrada de " << vf_area << " es: "<<raiz;
+	return 0;
+}
