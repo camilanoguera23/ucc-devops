ba011b1113619087b91e7ff698446a91c15e2596
Camila Andrea Noguera Poveda <camilanoguera23@gmail.com>
2021-10-21 20:24:32 -0500

    se separo

--- programa.cpp
+++ programa.cpp
@@ -84,6 +84,6 @@ int main() {
 	//Archivo funcion
 	//vf_area calcular raiz cuadrada en una variable e imprimir el resultado
 	raiz=sqrt(vf_area);
-	cout <<"La raiz cuadrada de" <<vf_area<< "es: "<<raiz;
+	cout <<"La raiz cuadrada de " << vf_area << " es: "<<raiz;
 	return 0;
 }
